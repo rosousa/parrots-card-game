fb07fbd483c37e41a7b413783a6eee5f7afb6143
rosousa <rodolfozsousa@gmail.com>
2022-06-16 18:38:12 -0300

    Game Logic Finished

--- scripts/script.js
+++ scripts/script.js
@@ -3,26 +3,37 @@ const imgArray = ['./assets/bobrossparrot.gif', './assets/explodyparrot.gif',
                  './assets/fiestaparrot.gif', './assets/metalparrot.gif', 
                  './assets/revertitparrot.gif', './assets/tripletsparrot.gif', 
                  './assets/unicornparrot.gif'];
+let hasFlipped = false;
+let lockMove = false;
+let firstCard, secondCard;
+let moves = 0;
 
 function start(){
-    const numberCards = prompt('Com quantas cartas gostaria de jogar?');
+    let numberCards;
     const cardsBoard = [];
     memory_game.innerHTML = '';
-    if(numberCards % 2 === 0 && numberCards >= 4 && numberCards <= 14){
-        for(let i = 0; i < numberCards / 2; i++){
-            cardsBoard.push(imgArray[i]);
-            cardsBoard.push(imgArray[i]);
-        }
-        cardsBoard.sort(randomize);
-        for(let i = 0; i < numberCards; i++){
-            memory_game.innerHTML += `<div class="card">
-                                        <img class="front-face" src="./assets/front.png" alt="front-face">
-                                        <img class="back-face" src="${cardsBoard[i]}" alt="back-face">
-                                      </div>`;
+
+    while(true){
+        numberCards = prompt('Digite o número de cartas');
+        if(numberCards % 2 === 0 && numberCards >= 4 && numberCards <= 14){
+            break;
+        } else {
+            alert('Digite um número par entre 4 e 14!');
         }
     }
-    else {
-        alert('Escolha um número de cartas correto!');
+
+    for(let i = 0; i < numberCards / 2; i++){
+        cardsBoard.push(imgArray[i]);
+        cardsBoard.push(imgArray[i]);
+    }
+
+    cardsBoard.sort(randomize);
+
+    for(let i = 0; i < numberCards; i++){
+        memory_game.innerHTML += `<div class="card">
+                                    <img class="front-face" src="./assets/front.png" alt="front-face">
+                                    <img class="back-face" src="${cardsBoard[i]}" alt="back-face">
+                                    </div>`;
     }
 }
 
@@ -31,7 +42,21 @@ start();
 const cards = document.querySelectorAll('.card');
 
 function flipCard(){
-    this.classList.toggle('flip');
+    if(lockMove){
+        return;
+    }
+    if(this === firstCard){
+        return;
+    }
+    this.classList.add('flip');
+    if(!hasFlipped){
+        hasFlipped = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlipped = false;
+    checkForMatch();
 }
 
 function randomize(){
@@ -39,3 +64,40 @@ function randomize(){
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+function checkForMatch(){
+    if(firstCard.innerHTML === secondCard.innerHTML){
+        countMoves();
+        disableCards();
+        return;
+    }
+    countMoves();
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetMoves();
+}
+
+function unflipCards(){
+    lockMove = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetMoves();
+    }, 1000);
+}
+
+function resetMoves(){
+    hasFlipped = false;
+    lockMove = false;
+    firstCard = '';
+    secondCard = '';
+}
+
+function countMoves(){
+    moves++;
+    console.log(moves);
+}
