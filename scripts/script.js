42b5b9cbe6421e23128a5e765ffced0ad79df588
rosousa <rodolfozsousa@gmail.com>
2022-06-16 16:37:32 -0300

    Add Random Cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,24 @@
 const memory_game = document.querySelector('.memory-game');
-
 const imgArray = ['./assets/bobrossparrot.gif', './assets/explodyparrot.gif',
                  './assets/fiestaparrot.gif', './assets/metalparrot.gif', 
-                 './assets/revertitparrot.gif', './assets/revertitparrot.gif', 
+                 './assets/revertitparrot.gif', './assets/tripletsparrot.gif', 
                  './assets/unicornparrot.gif'];
 
 function start(){
     const numberCards = prompt('Com quantas cartas gostaria de jogar?');
+    const cardsBoard = [];
     memory_game.innerHTML = '';
     if(numberCards % 2 === 0 && numberCards >= 4 && numberCards <= 14){
+        for(let i = 0; i < numberCards / 2; i++){
+            cardsBoard.push(imgArray[i]);
+            cardsBoard.push(imgArray[i]);
+        }
+        cardsBoard.sort(randomize);
         for(let i = 0; i < numberCards; i++){
             memory_game.innerHTML += `<div class="card">
                                         <img class="front-face" src="./assets/front.png" alt="front-face">
-                                        <img class="back-face" src="./assets/bobrossparrot.gif" alt="back-face">
-                                      </div>`
+                                        <img class="back-face" src="${cardsBoard[i]}" alt="back-face">
+                                      </div>`;
         }
     }
     else {
@@ -27,6 +32,10 @@ const cards = document.querySelectorAll('.card');
 
 function flipCard(){
     this.classList.toggle('flip');
+}
+
+function randomize(){
+    return Math.random() - 0.5;
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
