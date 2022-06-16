ee73ec905888c7e3c868d1527c84b65850d20033
rosousa <rodolfozsousa@gmail.com>
2022-06-16 16:00:58 -0300

    Add Cards Generate

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const memory_game = document.querySelector('.memory-game');
+
+const imgArray = ['./assets/bobrossparrot.gif', './assets/explodyparrot.gif',
+                 './assets/fiestaparrot.gif', './assets/metalparrot.gif', 
+                 './assets/revertitparrot.gif', './assets/revertitparrot.gif', 
+                 './assets/unicornparrot.gif'];
+
+function start(){
+    const numberCards = prompt('Com quantas cartas gostaria de jogar?');
+    memory_game.innerHTML = '';
+    if(numberCards % 2 === 0 && numberCards >= 4 && numberCards <= 14){
+        for(let i = 0; i < numberCards; i++){
+            memory_game.innerHTML += `<div class="card">
+                                        <img class="front-face" src="./assets/front.png" alt="front-face">
+                                        <img class="back-face" src="./assets/bobrossparrot.gif" alt="back-face">
+                                      </div>`
+        }
+    }
+    else {
+        alert('Escolha um número de cartas correto!');
+    }
+}
+
+start();
+
+const cards = document.querySelectorAll('.card');
+
+function flipCard(){
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
